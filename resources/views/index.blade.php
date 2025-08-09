fe9d1b5c626060dde615c9f6c0b1523555bfe0a2
RAMDHIL <158790363+RAMDHIL@users.noreply.github.com>
2025-08-09 22:02:12 +0700

    Update index.blade.php
    Add vite js

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Portofolio Fadhilah Ramadhan</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css','resource/js/app.js'])
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css"
         integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ=="
